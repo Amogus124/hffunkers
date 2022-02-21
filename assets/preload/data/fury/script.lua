049f61d125808459ea81f82f19f5cb9cd4b0ead0
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-02-21 13:08:02 -0300

    Add files via upload

---
+++ assets/preload/data/fury/script.lua
@@ -0,0 +1,7 @@
+function onCreate()
+        setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-htf');
+        setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'bunfriend_death_sound');
+        setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameover1');
+        setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameover2');
+
+end
